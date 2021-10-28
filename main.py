8e75b2f2ab8d45a59b57c6c057ee1fe73ff0d51c
German Kaztro <00229017@uca.edu.sv>
2021-10-28 05:36:02 +0000

    All UwU

--- main.py
+++ main.py
@@ -1,108 +1,121 @@
 # ------------------------------------------------------------
-# Lexer para C#
+# Lexer para C
 # ------------------------------------------------------------
 import ply.lex as lex
 
 # List of token names.   This is always required
-tokens = ('NUMBER', 
-          'PLUS', 
-          'MINUS', 
-          'TIMES', 
-          'DIVIDE', 
-          'LPAREN', 
-          'RPAREN',
-          'keyword', 
-          'identificador', 
-          'inicioBloque', 
-          'finBloque',
-          'finInstruccion', 
-          'asignacion', 
-          'comentario', 
-          'comentario_bloque',
-          'cadena', 
-          'coma')
+tokens = (
+   'NUMBER',
+   'operators',
+   'logicOperators',
+   'relationalOperators',
+   #'PLUS',
+   #'MINUS',
+   #'TIMES',
+   #'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+   'keyword',
+   'identificador',
+   'inicioBloque',
+   'finBloque',
+   'finInstruccion',
+   'asignacion',
+   'comentario',
+   'comentario_bloque',
+   'cadena',
+   'coma',
+   'punto'
+)
 
 # Regular expression rules for simple tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+#t_PLUS    = r'\+'
+#t_MINUS   = r'-'
+#t_TIMES   = r'\*'
+#t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
 t_inicioBloque = r'\{'
 t_finBloque = r'\}'
 t_finInstruccion = r'\;'
 t_asignacion = r'\='
-t_coma = r'\,'
-t_punto = r'\.'
-
+t_coma= r'\,'
+t_punto=r'\.'
 
 # A regular expression rule with some action code
+
+def t_relationalOperators(t):
+  r'(\==)|(\<)|(\>)|(\!=)|(\>=)|(\<=)'
+  return t
+
+def t_operators(t):
+  r'(\+)|(\-)|(\*)|(\/)|(\%)|(\==)|(\+{2})|(\-{2})'
+  return t
+
+def t_logicOperators(t):
+  r'(\|{2})|(\&{2})|(\!)'
+  return t
+
 def t_NUMBER(t):
     r'\d+'
-    t.value = int(t.value)
+    t.value = int(t.value)    
     return t
-
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-
 # A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
+t_ignore  = ' \t'
 
-
-def keywordToken(t):
-    r'(int)|(float)|(char)|(return)|(if)|(else)|(do)|(while)|(for)|(void)|(using)'
+def t_keyword(t):
+    r'(int)|(float)|(char)|(return)|(if)|(else)|(do)|(while)|(for)|(void)|(using)|(WriteLine)|(enum)|(true)|(switch)|(try)|(catch)'
     return t
 
-
-def identifierToken(t):
+def t_identificador(t):
     r'([a-z]|[A-Z]|_)([a-z]|[A-Z]|\d|_)*'
     return t
 
-
-def stringToken(t):
+def t_cadena(t):
     r'\".*\"'
     return t
 
-
-def commentToken(t):
+def t_comentario(t):
     r'\/\/.*'
     return t
 
-
-def blockCommentToken(t):
+def t_comentario_bloque(t):
     r'\/\*(.|\n)*\*\/'
     #return t
 
-
 # Error handling rule
-def errorToken(t):
+def t_error(t):
     print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+    t.lexer.skip(1)    
     return t
-
 
 # Build the lexer
 lexer = lex.lex()
 
-
 def miLexer():
-    f = open('fuente.c', 'r')
+    f = open('fuente.c','r')
     #lexer.input('3+4*_a23+-20*2')
     lexer.input(f.read())
     while True:
-        tok = lexer.token()
+        tok=lexer.token()
         if not tok:
             break
         #print(tok)
-        print(' type: ', tok.type, '\n', 'Value: ', tok.value, '\n',
-              'line #: ', tok.lineno, '\n', 'character #: ', tok.lexpos)
+        print(' type: ',tok.type,'\n',
+         'Value: ',tok.value,'\n',
+          'line #: ',tok.lineno,'\n',
+            'character #: ',tok.lexpos)
         print('---------------')
 
 
 if __name__ == "__main__":
   miLexer()
+        
+        
+        
